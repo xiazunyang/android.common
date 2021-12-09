a7a2f3a19ebc768c8f6003d79e4ec9e2b661ea9d
Tear丶残阳 <605658769@qq.com>
2021-12-09 21:37:32 +0800

    1.Android SDK升级至31。 2.Kotlin升级到1.6.0。 3.AppRuntime中添加用于获取metaData的Bundle实例。

--- android.common/src/main/java/cn/numeron/android/AppRuntime.kt
+++ android.common/src/main/java/cn/numeron/android/AppRuntime.kt
@@ -1,8 +1,11 @@
 package cn.numeron.android
 
 import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import java.util.concurrent.Executor
 
 object AppRuntime {
 
@@ -12,26 +15,35 @@ object AppRuntime {
 
     private lateinit var gMainExecutor: MainExecutor
 
+    /** 获取AndroidManifest.xml中的配置 */
+    val metaData = Bundle()
+
+    /** 返回[Context]实例 */
     val context: Context
         get() = gContext
 
+    /** 返回[Handler]实例。 */
     val handle: Handler
         get() {
-            if (!AppRuntime::gHandle.isInitialized) {
+            if (!::gHandle.isInitialized) {
                 gHandle = Handler(Looper.getMainLooper())
             }
             return gHandle
         }
 
+    /** 返回运行在主线程上的[Executor]实例 */
     val mainExecutor: MainExecutor
         get() {
-            if (!AppRuntime::gMainExecutor.isInitialized) {
+            if (!::gMainExecutor.isInitialized) {
                 gMainExecutor = MainExecutor(handle)
             }
             return gMainExecutor
         }
 
     fun init(context: Context) {
+        context.packageManager
+            .getApplicationInfo(context.packageName, PackageManager.GET_META_DATA)
+            .metaData?.let(metaData::putAll)
         if (!AppRuntime::gContext.isInitialized) {
             gContext = GlobalContext(context)
         }

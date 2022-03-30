5502099042e978e2ce26a4fc389927d5a0df9cf7
Tear丶残阳 <x.z.y.seido@outlook.com>
2022-03-30 10:00:29 +0800

    移除`Density`工具中的误差。

--- android.common/src/main/java/cn/numeron/android/Density.kt
+++ android.common/src/main/java/cn/numeron/android/Density.kt
@@ -4,23 +4,21 @@ package cn.numeron.android
 
 
 /* dp/px 相关工具 */
-private val density by lazy {
-    AppRuntime.context.resources.displayMetrics.density
-}
+private val density: Float
+    get() = AppRuntime.context.resources.displayMetrics.density
 
 val Int.dp: Int
-    get() = (density * this + 0.5f).toInt()
+    get() = (density * this).toInt()
 
 val Float.dp: Float
-    get() = density * this + 0.5f
+    get() = density * this
 
 /* sp/px 相关工具 */
-private val scaledDensity by lazy {
-    AppRuntime.context.resources.displayMetrics.scaledDensity
-}
+private val scaledDensity: Float
+    get() = AppRuntime.context.resources.displayMetrics.scaledDensity
 
 val Int.sp: Int
-    get() = (scaledDensity * this + 0.5f).toInt()
+    get() = (scaledDensity * this).toInt()
 
 val Float.sp: Float
-    get() = scaledDensity * this + 0.5f
+    get() = scaledDensity * this
